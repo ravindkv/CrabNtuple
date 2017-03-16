76fbc31856eed93e41d0f1e2f012c66c069016c0
Ravindra Verma <ravindra.kumar.verma@cern.ch>
2017-03-16 12:54:24 +0100

    modified

--- python/SamplesKeyValue.py
+++ python/SamplesKeyValue.py
@@ -8,7 +8,7 @@ def getMCVal(dict_MC, n):
 
 def getMCKey(dict_MC, n):
    val = getMCVal(dict_MC, n)
-   today_date = datetime.date.today()
+   today_date = str(datetime.date.today()).replace("-","")
    if val !="":
        start_name = val.split("/")[1].split("_")[0]
        key = start_name+"_ntuple_"+str(today_date)
@@ -25,11 +25,38 @@ def getDataVal(dict_Data, n):
 
 def getDataKey(dict_Data, n):
    val = getDataVal(dict_Data, n)
-   today_date = datetime.date.today()
+   today_date = str(datetime.date.today()).replace("-","")
    if val !="":
        start_name = val.split("/")[1].split("_")[0]
        key = start_name+"_ntuple_"+str(today_date)
        return key
+
+#PATHS OF NTUPLE AT T2_IN_TIFR
+def getPathsAtT2(samp_channel, samp_dict, n):
+    '''
+    /cms/store/user/rverma/
+    test/CrabMuonsMC/QCD_ntuple_2017-03-15_MuonsMC/
+    QCD_Pt-120to170_EMEnriched_TuneCUETP8M1_13TeV_pythia8/
+    crab_QCD_ntuple_2017-03-15_MuonsMC/170315_214120/
+    0000/W1JetsToLNu_ntuple_2017-03-15_muons_98.root
+    '''
+    local_crab_dir = 'Crab' +samp_channel
+    local_crab_subdir = getMCKey(samp_dict, n) +"_"+samp_channel
+    t2_user_dir = '/store/user/rverma'
+    t2_crab_dir = '/test/'+local_crab_dir+'/'+local_crab_subdir
+    t2_samp_name = "/"+getMCVal(samp_dict, n).split("/")[1]
+    t2_samp_subdir = "/crab_"+local_crab_subdir
+
+    year_full = str(datetime.date.today()).split("-")[0]
+    year_short = year_full.replace(year_full[0],"").replace(year_full[1],"")
+    month = str(datetime.date.today()).split("-")[1]
+    day = str(datetime.date.today()).split("-")[2]
+    t2_submit_date = "/"+year_short+ month+ day
+    t2_submit_time = "_"+str(datetime.datetime.now().time()).replace(":","").split(".")[0]
+    t2_submit_date_time = t2_submit_date + t2_submit_time
+    t2_full_path = '/cms'+ t2_user_dir+ t2_crab_dir+ t2_samp_name+ t2_samp_subdir+ t2_submit_date_time
+
+    return t2_full_path
 
 #NICE WAY TO PRINT STRINGS
 def toPrint(string, value):
--- test/CrabAllSamples_cfg.py
+++ test/CrabAllSamples_cfg.py
@@ -9,7 +9,7 @@ from AllDataSamples import dataSampDict
 from AllMCSamples import mcSampDict
 
 sys.path.insert(0, os.getcwd().replace("test","python"))
-from SamplesKeyValue import getMCVal, getMCKey, getDataVal, getDataKey, toPrint
+from SamplesKeyValue import getMCVal, getMCKey, getDataVal, getDataKey, getPathsAtT2, toPrint
 
 #PRINT SAMPLE INFO
 toPrint("Total MC samples",len(mcSampDict))
@@ -34,7 +34,6 @@ range_MC = 2
 range_Data = 2
 #-------------------------------
 
-
 #default CRAB parameters
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile#CRAB_configuration_parameters
 from CRABClient.UserUtilities import config, getUsernameFromSiteDB
@@ -51,40 +50,31 @@ config.Site.storageSite = 'T2_IN_TIFR'
 
 #https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRABClientLibraryAPI#Example_submitting_multiple_task
 
-file_of_t2_paths = open("PathsAtT2_"+str(datetime.date.today())+".py", 'w')
+file_of_t2_paths = open("FileOfT2Paths_"+ str(datetime.date.today()).replace("-","") +".py", 'w')
 #Muon channel
+all_t2_paths = []
 from CRABAPI.RawCommand import crabCommand
 if isMuons:
+    muons_t2_paths = []
     if isMC:
+        muons_MC_t2_paths = []
         toPrint("MINIAOD TO NTUPLE OF MC, FOR MUON CHANNEL","")
         file_of_t2_paths.write("MUON_MC\n")
         for m in range(range_MC):
             mu_MC = "MuonsMC"
-            local_crab_dir = 'Crab' +mu_MC
-            local_crab_subdir = getMCKey(mcSampDict, m) +"_"+mu_MC
-
-            #dirName of the output Ntuples at T2
-            config.General.requestName = local_crab_subdir
-            config.General.workArea = local_crab_dir
-            #fileName of output Ntuples at T2 are taken form psetName, e.g process.TFileService.fileName
+            config.General.requestName = getMCKey(mcSampDict, m) +"_"+mu_MC
+            config.General.workArea = 'Crab' +mu_MC
             config.JobType.psetName = 'muons_miniAOD_to_ntuple_13TeV_cfg.py'
             #specify the dataset. Comment process.source.fileNames in psetName
-            config.Data.inputDataset = str(getMCVal(mcSampDict, m))
-            #dir(/cms/store/user/rverma/) at T2
+            config.Data.inputDataset = getMCVal(mcSampDict, m)
+            config.Data.outLFNDirBase= '/store/user/rverma/test/Crab'+mu_MC+'/'+getMCKey(mcSampDict, m)+"_"+mu_MC
 
-            t2_user_dir = '/store/user/rverma'
-            t2_crab_dir = '/test/Crab'+mu_MC+'/'+local_crab_subdir
-            t2_samp_name = "/"+getMCVal(mcSampDict, m).split("/")[1]
-            t2_samp_subdir = "/crab_"+local_crab_subdir
-            t2_crab_submit_time = "/"+"s"
-            config.Data.outLFNDirBase = t2_user_dir+t2_crab_dir
-
-            t2_full_path = '/cms'+ t2_user_dir+ t2_crab_dir+ t2_samp_name+ t2_samp_subdir+ t2_crab_submit_time
-            toPrint("Path at T2", '/cms'+ t2_user_dir+ t2_crab_dir)
-            file_of_t2_paths.write(t2_full_path+"\n")
+            toPrint("Path at T2", '/cms'+ str(config.Data.outLFNDirBase))
+            muons_MC_t2_paths.append(getPathsAtT2(mu_MC, mcSampDict, m))
             crabCommand('submit', config = config)
 
     if isData:
+        muons_Data_t2_paths = []
         toPrint("MINIAOD TO NTUPLE OF DATA, FOR MUON CHANNEL","")
         for d in range(range_Data):
             mu_Data = "MuonsData"
@@ -97,10 +87,14 @@ if isMuons:
             toPrint("Path at T2",path_T2)
             file_path_T2.write(path_T2+getMCVal(mcSampDict, m).split("/")[1])
             #crabCommand('submit', config = config)
+    muons_t2_paths.append(muons_MC_t2_paths)
+    muons_t2_paths.append(muons_Data_t2_paths)
 
 #Electron channel
 if isElectrons:
+    electrons_t2_paths = []
     if isMC:
+        electrons_MC_t2_paths = []
         toPrint("MINIAOD TO NTUPLE OF MC, FOR ELECTRON CHANNEL","")
         for m in range(range_MC):
             ele_MC = "ElectronsMC"
@@ -114,6 +108,7 @@ if isElectrons:
             #crabCommand('submit', config = config)
 
     if isData:
+        electrons_Data_t2_paths = []
         toPrint("MINIAOD TO NTUPLE OF DATA, FOR ELECTRON CHANNEL","")
         for d in range(range_Data):
             ele_Data = "ElectronsData"
@@ -125,4 +120,10 @@ if isElectrons:
             config.Data.outLFNDirBase = path_T2
             toPrint("Path at T2",path_T2)
             #crabCommand('submit', config = config)
+    electrons_t2_paths.append(electrons_MC_t2_paths)
+    electrons_t2_paths.append(electrons_Data_t2_paths)
+
+all_t2_paths.append(muons_t2_paths)
+all_t2_paths.append(electrons_t2_paths)
+file_of_t2_paths.write(all_t2_paths)
 
