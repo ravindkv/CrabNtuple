b5f6c5742a058d10762afed0391a11a3c5475019
Ravindra Verma <ravindra.kumar.verma@cern.ch>
2017-03-30 11:21:09 +0200

    added

--- ntuple/statusMultiCrab.py
+++ ntuple/statusMultiCrab.py
@@ -18,23 +18,9 @@ from sampleKeyVal import *
 
 #Check availability of samples on DAS
 def execme(cmd):
-    print "\033[01;32m"+ "Excecuting: "+ "\033[00m",  cmd
+    #toPrint("\033[01;32m"+ "Excecuting: "+ "\033[00m",  cmd)
     os.system(cmd)
-toPrint("Total MC samples",len(mc))
-for n in range(len(mc)):
-    #print getMCKey(mc, n)
-    print getMCVal(mc, n)
-    das = "das_client.py --limit=1 --query=\"file dataset=%s\"" %getMCVal(mc, n)
-    #execme(das)
 
-toPrint("Total DATA samples",len(data))
-for n in range(len(data)):
-    #print getDataKey(data, n)
-    print getDataVal(data, n)
-    das = "das_client.py --limit=1 --query=\"file dataset=%s\"" %getDataVal(data, n)
-    #execme(das)
-
-#-------------------------------
 #USERS INPUTS
 isMu = True
 isEle = True
@@ -42,23 +28,37 @@ isMC = True
 isData = True
 range_MC = len(mc)
 range_Data = len(data)
-#-------------------------------
+#range_MC = 1
+#range_Data = 1
 
-if isMu:
-    if isMC:
-        for m in range(range_MC):
-            muMC_T2Paths.append(getNtupleT2Paths(mu_MC, mc, m))
+def statusMuMC(mc, m):
+    crab_dir = "CrabMuMC_20170329"
+    crab_subdir = "crab_"+getMCKey(mc, m)+"_MuMC_"+crab_dir.split("_")[1]
+    execme("crab status "+crab_dir+"/"+crab_subdir)
 
-    if isData:
-        for d in range(range_Data):
-            muData_T2Paths.append(getNtupleT2Paths(mu_Data, data, d))
+def statusMuData(data, d):
+    crab_dir = "CrabMuData_20170329"
+    crab_subdir = "crab_"+getDataKey(data, d)+"_MuData_"+crab_dir.split("_")[1]
+    execme("crab status "+crab_dir+"/"+crab_subdir)
 
-if isEle:
-    if isMC:
-        for m in range(range_MC):
-            electrons_MC_t2_paths.append(getNtupleT2Paths(ele_MC, mc, m))
+def statusEleMC(mc, m):
+    crab_dir = "CrabEleMC_20170329"
+    crab_subdir = "crab_"+getMCKey(mc, m)+"_EleMC_"+crab_dir.split("_")[1]
+    execme("crab status "+crab_dir+"/"+crab_subdir)
 
-    if isData:
-        for d in range(range_Data):
-            multiCrabSubmit(config, config.Data.outLFNDirBase)
+def statusEleData(data, d):
+    crab_dir = "CrabEleData_20170329"
+    crab_subdir = "crab_"+getDataKey(data, d)+"_EleData_"+crab_dir.split("_")[1]
+    execme("crab status "+crab_dir+"/"+crab_subdir)
+
+toPrint("Total MC samples",len(mc))
+for m in range(range_MC):
+    statusMuMC(mc, m)
+    statusEleMC(mc, m)
+
+toPrint("Total DATA samples",len(data))
+for d in range(range_Data):
+    statusMuData(data, d)
+    statusEleData(data, d)
+
 
