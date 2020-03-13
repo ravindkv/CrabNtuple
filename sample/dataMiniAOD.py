b250809f51abdc4d46d72141d70dd4f179cd513a
Ravindra Verma <ravindra.kumar.verma@cern.ch>
2020-03-13 06:20:47 +0100

    added signal samples

--- sample/dataMiniAOD.py
+++ sample/dataMiniAOD.py
@@ -2,19 +2,6 @@ import FWCore.ParameterSet.Config as cms
 from collections import OrderedDict
 
 dataSampDict_ ={
-        "MuRunB2v2": "/SingleMuon/Run2016B-03Feb2017_ver2-v2/MINIAOD"}
-'''
-dataSampDict_ ={
-        "MuRunB2v2": "/SingleMuon/Run2016B-03Feb2017_ver2-v2/MINIAOD",
-        "MuRunCv1":  "/SingleMuon/Run2016C-03Feb2017-v1/MINIAOD",
-        "MuRunDv1":  "/SingleMuon/Run2016D-03Feb2017-v1/MINIAOD",
-        "MuRunEv1":  "/SingleMuon/Run2016E-03Feb2017-v1/MINIAOD",
-        "MuRunFv1":  "/SingleMuon/Run2016F-03Feb2017-v1/MINIAOD",
-        "MuRunGv1":  "/SingleMuon/Run2016G-03Feb2017-v1/MINIAOD",
-        "MuRunH2v1": "/SingleMuon/Run2016H-03Feb2017_ver2-v1/MINIAOD",
-        "MuRunH3v1": "/SingleMuon/Run2016H-03Feb2017_ver3-v1/MINIAOD"
-         }
-dataSampDict_ ={
         "MuRunB2v2": "/SingleMuon/Run2016B-03Feb2017_ver2-v2/MINIAOD",
         "MuRunCv1":  "/SingleMuon/Run2016C-03Feb2017-v1/MINIAOD",
         "MuRunDv1":  "/SingleMuon/Run2016D-03Feb2017-v1/MINIAOD",
@@ -32,5 +19,4 @@ dataSampDict_ ={
         "EleRunHver2v1": "/SingleElectron/Run2016H-03Feb2017_ver2-v1/MINIAOD",
         "EleRunHver3v1": "/SingleElectron/Run2016H-03Feb2017_ver3-v1/MINIAOD"
          }
-'''
 dataSampDict= OrderedDict(sorted(dataSampDict_.items(), key=lambda t: t[0]))
--- sample/mcMiniAOD.py
+++ sample/mcMiniAOD.py
@@ -4,6 +4,10 @@ from collections import OrderedDict
 # MC Samples of Excited Lepton & Bkg at 13 TeV
 RUN = "RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6"
 M = "MINIAODSIM"
+
+mcSampDict_ = {"ExLepMuMuZ_M250":"/ExcitedLepton_MuMuZ-250_TuneCUETP8M1_13TeV_pythia8/"+RUN+"-v2/"+M}
+'''
+#### Both channel
 mcSampDict_ = {"DYJetsToLL_M50":"/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+RUN+"_ext2-v1/"+M}
 mcSampDict_["DYJetsToLL_Pt50To100"]="/DYJetsToLL_Pt-50To100_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+RUN+"-v3/"+M
 mcSampDict_["DYJetsToLL_Pt100To250"]="/DYJetsToLL_Pt-100To250_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/"+RUN+"-v2/"+M
@@ -20,6 +24,37 @@ mcSampDict_["ST_tW_antitop"]="/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pyt
 mcSampDict_["ST_t_top"]="/ST_t-channel_top_4f_inclusiveDecays_TuneCUETP8M2T4_13TeV-powhegV2-madspin/"+RUN+"-v1/"+M
 mcSampDict_["ST_t_antitop"]="/ST_t-channel_antitop_4f_inclusiveDecays_TuneCUETP8M2T4_13TeV-powhegV2-madspin/"+RUN+"-v1/"+M
 mcSampDict_["ST_s"]="/ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1/"+RUN+"-v1/"+M
+
+mcSampDict_["ExLepMuMuZ_M250"]="/ExcitedLepton_MuMuZ-250_TuneCUETP8M1_13TeV_pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M500"]="/ExcitedLepton_MuMuZ-500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M750"]="/ExcitedLepton_MuMuZ-750_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M1000"]="/ExcitedLepton_MuMuZ-1000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M1250"]="/ExcitedLepton_MuMuZ-1250_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M1500"]="/ExcitedLepton_MuMuZ-1500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v3/"+M
+mcSampDict_["ExLepMuMuZ_M1750"]="/ExcitedLepton_MuMuZ-1750_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M2000"]="/ExcitedLepton_MuMuZ-2000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M2500"]="/ExcitedLepton_MuMuZ-2500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M3000"]="/ExcitedLepton_MuMuZ-3000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M3500"]="/ExcitedLepton_MuMuZ-3500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M4000"]="/ExcitedLepton_MuMuZ-4000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v3/"+M
+mcSampDict_["ExLepMuMuZ_M4500"]="/ExcitedLepton_MuMuZ-4500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepMuMuZ_M5000"]="/ExcitedLepton_MuMuZ-5000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+
+mcSampDict_["ExLepEEZ_M250"]="/ExcitedLepton_EEZ-250_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v3/"+M
+mcSampDict_["ExLepEEZ_M500"]="/ExcitedLepton_EEZ-500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M750"]="/ExcitedLepton_EEZ-750_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M1000"]="/ExcitedLepton_EEZ-1000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M1250"]="/ExcitedLepton_EEZ-1250_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M1500"]="/ExcitedLepton_EEZ-1500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M1750"]="/ExcitedLepton_EEZ-1750_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M2000"]="/ExcitedLepton_EEZ-2000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v3/"+M
+mcSampDict_["ExLepEEZ_M2500"]="/ExcitedLepton_EEZ-2500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M3000"]="/ExcitedLepton_EEZ-3000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M3500"]="/ExcitedLepton_EEZ-3500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M4000"]="/ExcitedLepton_EEZ-4000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v3/"+M
+mcSampDict_["ExLepEEZ_M4500"]="/ExcitedLepton_EEZ-4500_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+mcSampDict_["ExLepEEZ_M5000"]="/ExcitedLepton_EEZ-5000_TuneCUETP8M1_13TeV-pythia8/"+RUN+"-v2/"+M
+'''
 # Sort dictionaly w.r.t the keys
 mcSampDict= OrderedDict(sorted(mcSampDict_.items(), key=lambda t: t[0]))
 
